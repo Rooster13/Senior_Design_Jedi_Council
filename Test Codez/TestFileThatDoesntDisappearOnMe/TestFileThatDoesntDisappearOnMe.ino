92e323bbb24a99de5f78496dca23b5db30a1176e
Sabrina Thompson <sxt122230@utdallas.edu>
2015-03-22 23:10:19 -0500

    Codez that are playing sounds; not perfect yet, but has changes so it plays our sound files correctly!

--- Test Codez/TestFileThatDoesntDisappearOnMe/TestFileThatDoesntDisappearOnMe.ino
+++ Test Codez/TestFileThatDoesntDisappearOnMe/TestFileThatDoesntDisappearOnMe.ino
@@ -41,12 +41,13 @@
 ///////// BEGIN WAVEHC INSTANTIATIONS /////////
 SdReader card;    // This object holds the information for the card
 FatReader root;   // This holds the information for the filesystem on the card
- 
+FatReader f;      // This holds the information for the file we're play 
 WaveHC wave;      // This is the only wave (audio) object, since we will only play one at a time
-
+FatVolume vol;    // This holds the information for the partition on the card 
 ////////// BEGIN IMU INSTANTIATIONS ///////////
 float angles[3]; // yaw pitch roll
 float angles2[3];
+float highestAngle2[1];
 // Set the FreeSixIMU object
 FreeSixIMU sixDOF = FreeSixIMU();
 
@@ -55,17 +56,17 @@ FreeSixIMU sixDOF = FreeSixIMU();
 void sdErrorCheck(void)
 {
   if (!card.errorCode()) return;
-  Serial.print("\n\rSD I/O error: ");
-  Serial.print(card.errorCode(), HEX);
-  Serial.print(", ");
-  Serial.println(card.errorData(), HEX);
+//  Serial.print("\n\rSD I/O error: ");
+//  Serial.print(card.errorCode(), HEX);
+//  Serial.print(", ");
+//  Serial.println(card.errorData(), HEX);
   while(1);
 }
 
 ///////////// SETUP FUNCTION /////////////// 
 void setup() {
   // Prescale internal clock for slow-down (for use at 3.3V)
-  FatVolume vol;    // This holds the information for the partition on the card 
+ 
   CLKPR = 0x80;
   CLKPR = 0x1;
  
@@ -97,7 +98,7 @@ void setup() {
  
   //  if (!card.init(true)) { //play with 4 MHz spi if 8MHz isn't working for you
   if (!card.init()) {         //play with 8 MHz spi (default faster!)  
-    Serial.println("Card init. failed!");  // Something went wrong, lets print out why
+    //Serial.println("Card init. failed!");  // Something went wrong, lets print out why
     sdErrorCheck();
     while(1);                            // then 'halt' - do nothing!
   }
@@ -112,14 +113,14 @@ void setup() {
       break;                             // we found one, lets bail
   }
   if (part == 5) {                       // if we ended up not finding one  :(
-    Serial.println("No valid FAT partition!");
+    //Serial.println("No valid FAT partition!");
     sdErrorCheck();      // Something went wrong, lets print out why
     while(1);                            // then 'halt' - do nothing!
   }
   
   // Try to open the root directory
   if (!root.openRoot(vol)) {
-    Serial.println("Can't open root dir!"); // Something went wrong,
+    //Serial.println("Can't open root dir!"); // Something went wrong,
     while(1);                             // then 'halt' - do nothing!
   }
   
@@ -131,79 +132,80 @@ void setup() {
 void loop() {
   sixDOF.getEuler(angles);
   float anglesTotal[3];
-  float highestAngle;
-  float highestAngle2;
-  float changeInAngleTotals;
+  float highestAngle1[1];
+  float changeInAnglesTotals[1];
+  float value[5];
   
-  Serial.print(angles[0]); 
-  Serial.print(" | ");  
-  Serial.print(angles[1]);
-  Serial.print(" | ");
-  Serial.println(angles[2]);
-  delay(100); 
-  
+  value[0] = 30;
+  value[1] = 50;
+  value[2] = 100;
+  value[3] = 200;
+  value[4] = 20;
+    
   anglesTotal[0] = abs(angles[0] - angles2[0]); //Gives total change in x axis
   anglesTotal[1] = abs(angles[1] - angles2[1]); //Gives total change in y axis
   anglesTotal[2] = abs(angles[2] - angles2[2]); //gives total change in z axis
-  changeInAngleTotals = abs(highestAngle - highestAngle2);
-//  Serial.print("Angle Totals: ");   
-//  Serial.print(anglesTotal[0]);
-//  Serial.print(" | ");  
-//  Serial.print(anglesTotal[1]);
-//  Serial.print(" | ");
-//  Serial.println(anglesTotal[2]);
-//  delay(100); 
-  
   
   //Statement to decide what angle is greatest to decide what sound to play
   if(anglesTotal[0] > anglesTotal[1])
   {
      if(anglesTotal[0] > anglesTotal[2])
      {
-       highestAngle = anglesTotal[0]; //Angle change x was highest
+       highestAngle1[0] = anglesTotal[0]; //Angle change x was highest
      }
      else
      {
-       highestAngle = anglesTotal[2]; //Angle change z was highest
+       highestAngle1[0] = anglesTotal[2]; //Angle change z was highest
      }
   }
   else
   {
     if(anglesTotal[1] > anglesTotal[2]) 
     {
-      highestAngle = anglesTotal[1]; //Angle change y was highest
+      highestAngle1[0] = anglesTotal[1]; //Angle change y was highest
     }
     else
     {
-      highestAngle = anglesTotal[2]; //Angle change z was highest
+      highestAngle1[0] = anglesTotal[2]; //Angle change z was highest
     }
   }
   
-  if(highestAngle <=13 && changeInAngleTotals > 20)
+   changeInAnglesTotals[0] = abs(highestAngle1[0] - highestAngle2[0]);
+   Serial.println(highestAngle1[0]);
+   Serial.println(highestAngle2[0]);
+   Serial.println(changeInAnglesTotals[0]);
+   
+  if(highestAngle1[0] <= value[0] && changeInAnglesTotals[0] > value[4])
   {
-    //play clash
+    playfile("Clash1.WAV");
+    delay(200);
   }
   else
   {
-    if(highestAngle <=13)
+    if(highestAngle1[0] <=value[0] && changeInAnglesTotals[0] < value[4])
     {  
-      //code here for playing hum
+      playfile("Minus80.WAV");
+      delay(1000);
     }
-    if(highestAngle >13 && highestAngle <= 50)
+    if(highestAngle1[0] <= value[1] && highestAngle1[0] > value[0])
     {
-      //code here for play slow swing
+      playfile("Minus40.WAV");
+      delay(1000);
     }
-    if(highestAngle > 50 && highestAngle <= 100)
+    if(highestAngle1[0] <= value[2] && highestAngle1[0] > value[1])
     {
-      //play medium swing
+      playfile("Zero0.WAV");
+      delay(1000);
     }
-    if(highestAngle >100 && highestAngle <=200)
+    if(highestAngle1[0] <=value[3] && highestAngle1[0] > value[2])
     {
-      //play pretty fast swing
+      playfile("Plus40.WAV");
+      delay(1000);
     }
-    if(highestAngle > 200)
+    if(highestAngle1[0] > value[3])
     {
-      //play fastest swing
+      playfile("Plus80.WAV");
+      delay(1000);
     }
   }
   
@@ -211,8 +213,9 @@ void loop() {
   angles2[0] = angles[0]; //initializes x angle2 to the previous x angle for total
   angles2[1] = angles[1]; //initializes y angle2 to the previous y angle for total
   angles2[2] = angles[2]; //initializes z angle2 to the previous z angle for total
-  highestAngle2 = highestAngle; //initializes change so we know whether to play hum or clash
+  highestAngle2[0] = highestAngle1[0]; //initializes change so we know whether to play hum or clash
   // Use playcomplete("SOUND.WAV") or playfile("SOUND.WAV") to call a sound to play 
+  Serial.println(highestAngle2[0]);
   Serial.println();
   delay(100); // this delay is to simulate 100ms read increment
 }
@@ -231,7 +234,7 @@ void playcomplete(char *name) {
  
 void playfile(char *name) {
   // see if the wave object is currently doing something
-  FatReader f;      // This holds the information for the file we're play
+  
   if (wave.isplaying) {// already playing something, so stop it!
     wave.stop(); // stop it
   }
